f9782bfe6b0710dc9f47fe96becb62d66b2a1568
Mena <dayjacs03@gmail.com>
2025-11-18 20:45:09 -0600

    working on navigation set up

---
+++ src/components/nav.jsx
@@ -0,0 +1,33 @@
+import Cake from "./cake.png";
+import Login from "./profile.svg";
+import Bag from "./shoppingBag.svg";
+
+export function Navi() {
+  return (
+    <nav className="naviHolder">
+      <div className="preShop">
+        <div>
+          <p>Free shipping on all orders!</p>
+        </div>
+        <div>
+          <p className="">Track an Order</p>
+          <p className="">Gift Cards</p>
+        </div>
+      </div>
+      <div className="navi">
+        <div className="lNavi">
+          <img src={Cake} alt="white cake with strawberrys on top - logo"></img>
+          <div>
+            <p className="shopCategories">Brands</p>
+            <p className="shopCategories">Category</p>
+            <p className="shopCategories">Clean Beauty</p>
+          </div>
+        </div>
+        <div className="rNavi">
+          <img src={Login} alt="login icon"></img>
+          <img src={Bag} alt="shopping cart icon"></img>
+        </div>
+      </div>
+    </nav>
+  );
+}
