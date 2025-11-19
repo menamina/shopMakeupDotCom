ad31f0e320547248886a24ff44f729fc333d4ccf
Mena <dayjacs03@gmail.com>
2025-11-19 17:04:51 -0600

    finished logic for now for api i believe

--- src/App.jsx
+++ src/App.jsx
@@ -17,8 +17,7 @@ function App() {
   }
 
   function noNull(list) {
-    const noNull = list.filter(Boolean);
-    return noNull;
+    return list.filter((item) => item !== null && item !== "");
   }
 
   useEffect(() => {
@@ -28,15 +27,15 @@ function App() {
           "http://makeup-api.herokuapp.com/api/v1/products.json"
         );
         const result = await apiLink.json();
-        const productList = result.map((item) => item);
-        const brandList = result.map((item) => item.brand);
-        const categoryList = result.map((item) => item.category);
+        const productList = result;
+        const brandList = noDupes(noNull(result.map((item) => item.brand)));
+        const categoryList = noNull(result.map((item) => item.category));
         const tagList = noDupes(result.flatMap((item) => item.tag_list));
 
         updateProducts(productList);
-        updateBrands(noDupes(brandList));
-        updateCategories(noDupes(categoryList));
-        updateCleanBeauty(noNull(tagList));
+        updateBrands(brandList);
+        updateCategories(categoryList);
+        updateCleanBeauty(tagList);
       } catch (error) {
         updateApiErr(error);
       }
