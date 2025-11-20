9d51e9bb851621a7e91df3ec96213e6b6b4bcc21
Mena <dayjacs03@gmail.com>
2025-11-20 17:16:03 -0600

    working on css atm

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,56 +3,58 @@ import Email from "../imgs/email.svg";
 import Phone from "../imgs/phone.svg";
 
 export default function Footer() {
-  <div className="footerHolder">
-    <div className="foot1">
-      <img src={Cake} alt="cake brand logo"></img>
-      <div>
-        <img src={Email} alt="email logo"></img>
-        <input
-          type="email"
-          aria-label="enter email here"
-          placeholder="Enter your email"
-        />
+  return (
+    <div className="footerHolder">
+      <div className="foot1">
+        <img src={Cake} alt="cake brand logo"></img>
+        <div>
+          <img src={Email} alt="email logo"></img>
+          <input
+            type="email"
+            aria-label="enter email here"
+            placeholder="Enter your email"
+          />
+        </div>
+        <div className="emptyDiv"></div>
+        <div>
+          <img src={Phone} alt="phone logo"></img>
+          <input
+            type="email"
+            aria-label="enter phone number here"
+            placeholder="Enter phone number"
+          />
+        </div>
+        <div className="emptyDiv"></div>
       </div>
-      <div className="emptyDiv"></div>
-      <div>
-        <img src={Phone} alt="phone logo"></img>
-        <input
-          type="email"
-          aria-label="enter phone number here"
-          placeholder="Enter phone number"
-        />
+      <div className="foot2">
+        <p>COMPANY</p>
+        <p>About</p>
+        <p>Contact Us</p>
       </div>
-      <div className="emptyDiv"></div>
-    </div>
-    <div className="foot2">
-      <p>COMPANY</p>
-      <p>About</p>
-      <p>Contact Us</p>
-    </div>
-    <div className="foot3">
-      <p>HELP</p>
-      <p>FAQ</p>
-      <p>Return Policy</p>
-    </div>
-    <div className="foot4">
-      <p>FOLLOW US</p>
-      <div>
-        <img></img>
-        <p>Instagram</p>
+      <div className="foot3">
+        <p>HELP</p>
+        <p>FAQ</p>
+        <p>Return Policy</p>
       </div>
-      <div>
-        <img></img>
-        <p>X</p>
-      </div>
-      <div>
-        <img></img>
-        <p>Youtube</p>
+      <div className="foot4">
+        <p>FOLLOW US</p>
         <div>
           <img></img>
-          <p>Tiktok</p>
+          <p>Instagram</p>
+        </div>
+        <div>
+          <img></img>
+          <p>X</p>
+        </div>
+        <div>
+          <img></img>
+          <p>Youtube</p>
+          <div>
+            <img></img>
+            <p>Tiktok</p>
+          </div>
         </div>
       </div>
     </div>
-  </div>;
+  );
 }
--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -2,6 +2,7 @@ import Cake from "../imgs/cake.png";
 import Login from "../imgs/profile.svg";
 import Bag from "../imgs/shoppingBag.svg";
 import { Link } from "react-router";
+import styles from "../css/navi.module.css";
 
 export default function Navi({
   byBrand,
@@ -12,38 +13,43 @@ export default function Navi({
   cartTotal,
 }) {
   return (
-    <nav className="naviHolder">
-      <div className="preShop">
+    <nav className={styles.naviHolder}>
+      <div className={styles.preShop}>
         <div>
           <p>Free shipping on all orders!</p>
         </div>
-        <div>
-          <p className="">Track an Order</p>
-          <p className="">Gift Cards</p>
+        <div className={styles.TrackGift}>
+          <p className={styles.track}>Track an Order</p>
+          <p className={styles.gift}>Gift Cards</p>
         </div>
       </div>
-      <div className="navi">
-        <div className="lNavi">
-          <div className="logoNav">
+
+      <div className={styles.navi}>
+        <div className={styles.lNavi}>
+          <div className={styles.logoNav}>
             <img
+              className={styles.cake}
               src={Cake}
               alt="white cake with strawberrys on top - brand logo"
             ></img>
             <p>Cakeface.</p>
           </div>
           <div>
-            <p className="shopCategories" onClick={() => menuState("brands")}>
+            <p
+              className={styles.shopCategories}
+              onClick={() => menuState("brands")}
+            >
               Brands
             </p>
             <div
-              className={`dropdownBrands ${
-                isOpen === "brands" ? "show" : "hide"
+              className={`${styles.dropdownBrands} ${
+                isOpen === "brands" ? styles.show : styles.hidden
               }`}
             >
               {byBrand.map((brand) => (
                 <Link
                   to={`/Brand/${brand}`}
-                  className="dropdownItem"
+                  className={styles.dropdownItem}
                   key={brand}
                 >
                   {brand}
@@ -51,18 +57,21 @@ export default function Navi({
               ))}
             </div>
 
-            <p className="shopCategories" onClick={() => menuState("category")}>
+            <p
+              className={styles.shopCategories}
+              onClick={() => menuState("category")}
+            >
               Category
             </p>
             <div
-              className={`dropdownCategory ${
-                isOpen === "category" ? "show" : "hide"
+              className={`${styles.dropdownCategory} ${
+                isOpen === "category" ? styles.show : styles.hidden
               }`}
             >
               {byCategory.map((category) => (
                 <Link
                   to={`/Category/${category}`}
-                  className="dropdownItem"
+                  className={styles.category}
                   key={category}
                 >
                   {category}
@@ -70,20 +79,20 @@ export default function Navi({
               ))}
             </div>
             <p
-              className="shopCategories"
+              className={styles.shopCategories}
               onClick={() => menuState("clean beauty")}
             >
               Clean Beauty
             </p>
             <div
-              className={`dropdownClean ${
-                isOpen === "clean beauty" ? "show" : "hide"
+              className={`${styles.dropdownClean} ${
+                isOpen === "clean beauty" ? styles.show : styles.hidden
               }`}
             >
               {byCleanBeauty.map((tag) => (
                 <Link
                   to={`/CleanBeautyTags/${tag}`}
-                  className="dropdownItem"
+                  className={styles.tag}
                   key={tag}
                 >
                   {tag}
@@ -93,11 +102,15 @@ export default function Navi({
           </div>
         </div>
         <div className="rNavi">
-          <img src={Login} alt="login icon"></img>
+          <img className={styles.login} src={Login} alt="login icon"></img>
           <Link to={`/Cart`} className="cart">
             <div className="cartHolder">
               <div>{cartTotal === 0 ? "" : cartTotal}</div>
-              <img src={Bag} alt="shopping cart icon"></img>
+              <img
+                className={styles.bag}
+                src={Bag}
+                alt="shopping cart icon"
+              ></img>
             </div>
           </Link>
         </div>
